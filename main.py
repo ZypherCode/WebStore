0340475be9f23e38ff74b83cfdb38d2499e658ee
ZypherCode <dronovmiroslavik@gmail.com>
2026-05-11 19:05:36 +0300

    Deploy

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def index():
 
 def main():
     db_session.global_init("db/store.db")
-    app.run(host="127.0.0.1", port="8080")
+    app.run(host="0.0.0.0", port="8080")
 
 
 @app.errorhandler(403)
